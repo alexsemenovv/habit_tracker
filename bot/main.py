fae503e8d9763469d782951d9f9762aa3114cb6a
Aleksandr <aleksandrsemeonow@yandex.ru>
2024-12-18 12:34:10 +0500

    update requirements & ignore files

--- bot/main.py
+++
@@ -1,9 +0,0 @@
-from loader import bot
-from logging import DEBUG
-import logging
-
-logging.basicConfig(level=DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger = logging.getLogger(__name__)
-
-if __name__ == "__main__":
-    bot.infinity_polling()

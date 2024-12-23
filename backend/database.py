f519d9caf5d23de6989da5bf5db31325a9fdbafc
Aleksandr <aleksandrsemeonow@yandex.ru>
2024-12-23 20:05:43 +0500

    add alembic migration

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,9 @@ from typing import Any
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine, AsyncEngine
 from sqlalchemy.orm import declarative_base
 
-URL_DATABASE: str = "postgres+asyncpg://admin:admin@localhost:5433/habits"
+URL_DATABASE: str = "postgresql+asyncpg://admin:admin@localhost:5433/habits_db"
 engine: AsyncEngine = create_async_engine(URL_DATABASE, echo=True)
 async_session: async_sessionmaker[AsyncSession | Any] = async_sessionmaker(bind=engine, expire_on_commit=False,
                                                                            class_=AsyncSession)
 
-Base = declarative_base()
+Base: declarative_base = declarative_base()
